7b2f914521d098a9451fa05ae5d1df9e3a2a1df6
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-07 09:10:59 +0200

    intro to state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
 
 /**
- * 
+ *
  * BEM - Block Element Modifier:
  *  app__title
  *  app__title--large
  *  app__title--small
  */
 
+// Functional component
 function App() {
   const name = "Dimitar";
   const isMale = true;
 
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count -1);
+  };
   function getGreeting(user) {
     if (user) {
       return <h1>Hello, {user}</h1>;
@@ -22,11 +33,16 @@ function App() {
 
   return (
     <div className="app">
-      <Header/>
+      <Header />
       {getGreeting()}
-      <h2 className="app__title">You are a {isMale ? "Male" : "Female"} & this is a title</h2>
+      <h2 className="app__title">
+        You are a {isMale ? "Male" : "Female"} & this is a title
+      </h2>
       <h3 className="app__title app__title--large">This is a LARGE title</h3>
       <h3 className="app__title--small">This is a small title</h3>
+      <h2>The count is {count}</h2>
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
     </div>
   );
 }
