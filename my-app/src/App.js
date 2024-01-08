b5fd0ebfdd8ff9528c5222fc6f64c333e063ab4e
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-08 10:25:02 +0200

    useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,6 +15,8 @@ import Product from "./Product";
 function App() {
   const isMale = true;
 
+  // Adding the windowWidth just for later reference
+  // const [windowWidth, setWindowWidth] = useState(window.innerWidth)
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
@@ -29,6 +31,36 @@ function App() {
   2) Don't call hooks from regular JS functions
       - Call hooks from React FC's
       - Call hooks from custom hooks
+
+  3) useEffect
+      - On every render:
+
+        useEffect(() => {
+            console.log('I re-rendered');
+        })
+      
+      - On first Render/Mount only! - componentDidMount Alternative:
+
+        useEffect(() => {
+              console.log('The component Mounted');
+        }, [])
+      
+      - On first Render + whenever dependency changes! - componentDidUpdate Alternative:
+
+        useEffect(() => {
+                console.log(`The name changed!: ${name}`);
+          }, [name])
+
+      - Follows the same rules, except this handles the unmounting on a component! - componenWillUnmount Alternative:
+        
+        useEffect(() => {
+                window.addEventListener("resize", updateWindowWidth);
+                
+                // when component unmounts, this cleanup code runs...
+                return() => {
+                  window.removeEventListener("resize", updateWindowWidth)
+                }
+          }, [])
   */
 
   const addTodo = (e) => {
