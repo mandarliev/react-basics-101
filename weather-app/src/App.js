96053b7b2c959e30e6ca02722f38192d1d302d25
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-09 14:01:37 +0200

    imported axios and added the useEffect

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,56 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useEffect } from "react";
+
+import axios from "axios";
+
+const options = {
+  method: "GET",
+  url: "https://open-weather-map27.p.rapidapi.com/weather",
+  headers: {
+    "X-RapidAPI-Key": "0ca030f99amshf53c07229b47eb7p13a768jsn6c2df59a7595",
+    "X-RapidAPI-Host": "open-weather-map27.p.rapidapi.com",
+  },
+};
+
+try {
+  const response = await axios.request(options);
+  console.log(response.data);
+} catch (error) {
+  console.error(error);
+}
 
 function App() {
+  useEffect(() => {
+    axios
+      .request(options)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>Weather App</h1>
     </div>
   );
 }
