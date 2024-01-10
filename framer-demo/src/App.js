7dcfa89b5b07085e0b87f0bb8336f4470044999d
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-10 12:11:01 +0200

    added the motion lib

--- framer-demo/src/App.js
+++ framer-demo/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import { motion } from "framer-motion"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Framer Motion Demo</h1>
     </div>
   );
 }
