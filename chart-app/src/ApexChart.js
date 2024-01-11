6da5499339a19e2cc39d1e661aadddda59fc289f
Dimitar <dimitar.mandarliev@gmail.com>
2024-01-11 14:30:42 +0200

    turned ApexChart into functional component

--- chart-app/src/ApexChart.js
+++ chart-app/src/ApexChart.js
@@ -1,70 +1,64 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Chart from "react-apexcharts";
 
-class ApexChart extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      series: [
-        {
-          name: "Desktops",
-          data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
-        },
-      ],
-      options: {
-        chart: {
-          height: 350,
-          type: "line",
-          zoom: {
-            enabled: false,
-          },
-        },
-        dataLabels: {
+const ApexChart = () => {
+  const [chartConfig, setChartConfig] = useState({
+    series: [
+      {
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
           enabled: false,
         },
-        stroke: {
-          curve: "straight",
-        },
-        title: {
-          text: "Product Trends by Month",
-          align: "left",
-        },
-        grid: {
-          row: {
-            colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-            opacity: 0.5,
-          },
-        },
-        xaxis: {
-          categories: [
-            "Jan",
-            "Feb",
-            "Mar",
-            "Apr",
-            "May",
-            "Jun",
-            "Jul",
-            "Aug",
-            "Sep",
-          ],
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "straight",
+      },
+      title: {
+        text: "Product Trends by Month",
+        align: "left",
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
         },
       },
-    };
-  }
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+        ],
+      },
+    },
+  });
 
-  render() {
-    return (
-      <div id="chart">
-        <Chart
-          options={this.state.options}
-          series={this.state.series}
-          type="line"
-          height={350}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div id="chart">
+      <Chart
+        options={chartConfig.options}
+        series={chartConfig.series}
+        type="line"
+        height={350}
+      />
+    </div>
+  );
+};
 
 export default ApexChart;
